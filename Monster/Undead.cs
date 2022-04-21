17d04756872d96e8a970a67e68fbbb3fdd5ef064
inten <inten@DESKTOP-R2O3CVB>
2022-04-21 12:53:08 -0400

    Add project files.

---
+++ Monster/Undead.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monsters
+{
+    abstract class Undead : Creature
+    {
+        /* Fields */
+        /* Properties */
+        /* Constructors */
+
+        public Undead() : base()
+        {
+            this.Darkvision = true;
+            this.Resistances += "Necrotic ";
+        }
+
+        public bool Darkvision { get; private set; }
+        public string Resistances { get; private set; }
+        /* Methods */
+    }
+}
